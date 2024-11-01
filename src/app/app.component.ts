93f930614acf83f1ef1b668b22469c023c1db064
ThiagoSantos06 <thiagohenrique1983c@gmail.com>
2024-11-01 19:24:03 -0300

    Primeiro commi

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TarefaComponent } from "./componentes/tarefa/tarefa.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TarefaComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'todo-front';
+}
