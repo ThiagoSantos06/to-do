ef62aa12cd8eddf0f63f547422e509331f9907ae
ThiagoSantos06 <thiagohenrique1983c@gmail.com>
2024-11-13 20:40:50 -0300

    💄 feat: estiliza listagem das tarefas

--- src/app/componentes/tarefa/tarefa.component.ts
+++ src/app/componentes/tarefa/tarefa.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, } from '@angular/core';
+import { TarefaDTO } from '../../dto/TarefaDTO';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-tarefa',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule],
   templateUrl: './tarefa.component.html',
   styleUrl: './tarefa.component.css'
 })
 export class TarefaComponent {
-
+  @Input() tarefa!: TarefaDTO
 }
